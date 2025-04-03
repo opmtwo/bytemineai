4b73118a9ab0a1f7e14b8cf7d0c4b8b4ab051e32
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-04-03 17:13:08 +0530

    updated b2b etl script to transform and clean data as per our schema

--- scripts/bytemine-share-data.ts
+++ scripts/bytemine-share-data.ts
@@ -1,16 +1,75 @@
-import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
-import * as zlib from 'zlib';
-import { SdkStreamMixin } from '@smithy/types';
 import csv from 'csv-parser';
+import { stringify } from 'csv-stringify'; // Import correctly
 import fs from 'fs';
-import stream from 'stream';
 import { Readable } from 'stream';
 import { pipeline } from 'stream/promises';
-import { stringify as stringifySync } from 'csv-stringify/sync';
-import { stringify } from 'csv-stringify'; // Import correctly
+import * as zlib from 'zlib';
+
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { SdkStreamMixin } from '@smithy/types';
 
 // Create a CSV stringify stream
 const csvStringify = stringify({ header: true });
+
+const fieldMapping: Record<string, string> = {
+	first_name: 'first_name',
+	last_name: 'last_name',
+	middle_name: 'middle_name',
+	full_name: 'full_name',
+	job_title: 'job_title',
+	job_level: 'seniority',
+	job_function: 'department',
+	enhanced_job_function: 'primary_role',
+	job_sub_function: 'secondary_role',
+	linkedin_url: 'linkedin_profile',
+	linkedin_connection_count: 'linkedin_connection_count',
+	work_email: 'work_email',
+	work_email_array: 'work_email_history',
+	personal_email: 'personal_email',
+	personal_email_2: 'personal_email2',
+	personal_email_array: 'personal_email_history',
+	phone_1_dd: 'direct_dial',
+	phone_mobile: 'mobile_number',
+	phone_mobile_array: 'mobile_number_history',
+	phone_2_org: 'work_number',
+	education: 'education',
+	skills: 'skills',
+	interests: 'interests',
+	linkedin_headline: 'linkedin_headline',
+	facebook_url: 'facebook_profile',
+	twitter_url: 'twitter_profile',
+	work_experience: 'work_experience',
+	compensation: 'salary',
+	birth_year: 'birth_year',
+	approximate_age: 'age',
+	gender: 'gender',
+	recent_job_change: 'new_hire',
+	job_start_date: 'job_start_date',
+	org_name: 'company_name',
+	org_domain: 'company_domain',
+	org_website: 'company_website',
+	org_linkedin_url: 'company_linkedin_profile',
+	org_linkedin_followers: 'company_linkedin_followers',
+	address1_line1: 'company_local_address',
+	address1_city: 'company_local_city',
+	address1_state: 'company_local_state',
+	address1_postal: 'company_local_address_postal',
+	address2_line1: 'company_hq_address',
+	address2_city: 'company_hq_city',
+	address2_state: 'company_hq_state',
+	address2_postal: 'company_hq_postal',
+	address3_city: 'person_city',
+	address3_state: 'person_state',
+	address3_postal: 'person_postal',
+	employee_range: 'company_employee_range',
+	revenue_range: 'company_revenue_range',
+	industry: 'company_industry',
+	sic_4d: 'company_sic4',
+	sic_4d_desc: 'company_sic4_description',
+	naics: 'company_naics',
+	naics_desc: 'company_naics_description',
+	org_ownership_status: 'company_type',
+};
 
 async function gzipCsv(inputCsv: string, outputGz: string): Promise<void> {
 	try {
@@ -35,7 +94,7 @@ const OUTPUT_BUCKET = process.env.OUTPUT_BUCKET || 'bytemineai-b2b-data';
 const MOBILE_NUMBERS_BUCKET = process.env.MOBILE_NUMBERS_BUCKET || 'bytemineai-b2b-data';
 
 const INPUT_FILE_KEY = process.env.INPUT_FILE_KEY || 'salutary_bytemine_2025q1.csv.gz';
-const SHARED_FILE_KEY = process.env.SHARED_FILE_KEY || 'shared.csv.gz';
+const SHARED_FILE_KEY = process.env.SHARED_FILE_KEY || 'mobile-numbers-2025-Q2.csv.gz';
 
 async function getStreamFromS3(bucket: string, key: string): Promise<Readable> {
 	const command = new GetObjectCommand({ Bucket: bucket, Key: key });
@@ -46,7 +105,7 @@ async function getStreamFromS3(bucket: string, key: string): Promise<Readable> {
 	return Body as Readable & SdkStreamMixin;
 }
 
-async function readCsvFromS3(bucket: string, key: string, maxRecords?: number): Promise<Map<string, string>> {
+async function readCsvFromS3AsPidMobileNoMap(bucket: string, key: string, maxRecords?: number): Promise<Map<string, string>> {
 	const s3Stream = await getStreamFromS3(bucket, key);
 	const resultMap = new Map<string, string>();
 	let count = 0;
@@ -54,8 +113,8 @@ async function readCsvFromS3(bucket: string, key: string, maxRecords?: number): 
 
 	for await (const row of reader) {
 		if (maxRecords && count >= maxRecords) break;
-		if (row['phone_1_dd']) {
-			resultMap.set(row.pid, row['phone_1_dd']);
+		if (row['phone_1_dd'] || row['phone_mobile']) {
+			resultMap.set(row.pid, row['phone_1_dd'] || row['phone_mobile']);
 			count++;
 		}
 	}
@@ -63,8 +122,8 @@ async function readCsvFromS3(bucket: string, key: string, maxRecords?: number): 
 }
 
 async function processData(maxRecords?: number) {
-	const allMobileNumbers = await readCsvFromS3(INPUT_BUCKET, INPUT_FILE_KEY, maxRecords);
-	// const sharedMobileNumbers = await readCsvFromS3(SHARED_BUCKET, SHARED_FILE_KEY, maxRecords);
+	const allMobileNumbers = await readCsvFromS3AsPidMobileNoMap(INPUT_BUCKET, INPUT_FILE_KEY, maxRecords);
+	// const sharedMobileNumbers = await readCsvFromS3AsPidMobileNoMap(SHARED_BUCKET, SHARED_FILE_KEY, maxRecords);
 	const sharedMobileNumbers = new Map();
 
 	// Select 55% of non-shared numbers randomly
@@ -75,49 +134,14 @@ async function processData(maxRecords?: number) {
 	const availablePids = allPids.filter((pid) => !sharedPids.has(pid));
 	const randomPids = new Set(availablePids.sort(() => Math.random() - 0.5).slice(0, Math.ceil(availablePids.length * 0.55)));
 
+	const limit = Math.ceil(availablePids.length * 0.55); // 55% limit
 	for (const pid of randomPids) {
-		newSharePids.add(pid);
+		if (newSharePids.size < limit) {
+			newSharePids.add(pid);
+		}
 	}
 
-	// console.log({ allPids, sharedPids, newSharePids, availablePids, randomPids });
-	// return;
-
-	// // Process CSV and mask numbers that are not in newSharePids
-	// const outputFileName = `dataset-${new Date().getFullYear()}-Q${Math.ceil((new Date().getMonth() + 1) / 3)}.csv.gz`;
-	// const outputFileStream = fs.createWriteStream(outputFileName);
-	// const gzipStream = zlib.createGzip();
-
-	// const s3Stream = await getStreamFromS3(INPUT_BUCKET, INPUT_FILE_KEY);
-
-	// let processedCount = 0;
-	// const reader = s3Stream.pipe(zlib.createGunzip()).pipe(csv());
-	// const writer = new Readable({
-	//     objectMode: true,
-	//     read() {}
-	// });
-
-	// reader.on("data", (row) => {
-	//     if (processedCount >= (maxRecords ?? Infinity)) {
-	//         reader.destroy(); // End processing immediately when maxRecords is reached
-	//         writer.push(null);
-	//         return;
-	//     }
-	//     row.mobileNumber = newSharePids.has(row.pid) ? row.mobileNumber : "XXXXXXX";
-	//     writer.push(`${row.pid},${row.mobileNumber}\n`);
-	//     processedCount++;
-	// });
-
-	// reader.on("end", () => writer.push(null));
-
-	// await pipeline(writer, gzipStream, outputFileStream);
-
-	// // Upload processed file
-	// await s3.send(new PutObjectCommand({
-	//     Bucket: OUTPUT_BUCKET,
-	//     Key: outputFileName,
-	//     Body: fs.createReadStream(outputFileName),
-	//     ContentType: "application/gzip"
-	// }));
+	console.log({ allPids: allPids.length, sharedPids: sharedPids.size, availablePids: availablePids.length, newSharePids: newSharePids.size });
 
 	const outputFileName = `dataset-${new Date().getFullYear()}-Q${Math.ceil((new Date().getMonth() + 1) / 3)}.csv`;
 	const outputGzipFileName = `${outputFileName}.gz`;
@@ -128,7 +152,7 @@ async function processData(maxRecords?: number) {
 
 	let processedCount = 0;
 	reader.on('data', (row) => {
-        console.log('Processing row', processedCount + 1);
+		console.log('Processing row', processedCount + 1);
 		if (processedCount >= (maxRecords ?? Infinity)) {
 			reader.end(); // Stop processing
 			return;
@@ -136,92 +160,77 @@ async function processData(maxRecords?: number) {
 		processedCount++;
 
 		// Mask mobile number if necessary
-		row.mobileNumber = newSharePids.has(row.pid) ? row.mobileNumber : 'XXXXXXX';
+		// row.phone_1_dd = newSharePids.has(row.pid) ? row.phone_1_dd : '';
+		if (!newSharePids.has(row.pid)) {
+            console.log({ pid: row.pid });
+			row.phone_1_dd = '';
+			row.phone_mobile = '';
+			row.phone_mobile_array = '';
+		}
+
+		// Transform the records to our own schema
+		const transformedRecord: Record<string, any> = {};
+		Object.keys(fieldMapping).forEach((key) => {
+			if (row[key] !== undefined) {
+				transformedRecord[fieldMapping[key]] = row[key];
+			} else {
+				row[key] = '';
+			}
+		});
 
 		// Write the formatted row
-		csvStringify.write(row);
+		// csvStringify.write(row);
+		csvStringify.write(transformedRecord);
 	});
-    
-    // End the CSV writing properly
-    const waitForRead = () => new Promise((resolve, reject) => {
-            reader.on("end", () => {
-            csvStringify.end();
-            console.log("CSV file read successfully.");
-            resolve({});
-        });
-    });
 
-    await waitForRead();
-    
-    // Pipe the CSV formatted output to the file
-    const waitForWrite = () => new Promise((resolve, reject) => {
-        csvStringify.pipe(outputFileStream).on('finish', () => {
-            console.log("CSV file written successfully.");
-            resolve({});
-        })
-    });
+	// End the CSV writing properly
+	const waitForRead = () =>
+		new Promise((resolve, reject) => {
+			reader.on('end', () => {
+				csvStringify.end();
+				console.log('CSV file read successfully.');
+				resolve({});
+			});
+		});
 
-    await waitForWrite();
+	await waitForRead();
 
-    // -------------------------------------------------------------------------
+	// Pipe the CSV formatted output to the file
+	const waitForWrite = () =>
+		new Promise((resolve, reject) => {
+			csvStringify.pipe(outputFileStream).on('finish', () => {
+				console.log('CSV file written successfully.');
+				resolve({});
+			});
+		});
 
-    // End the CSV writing properly
-    // reader.on("end", () => {
-    //     csvStringify.end();
-    //     console.log("CSV file written successfully.");
-    // });
-    
-    // Pipe the CSV formatted output to the file
-    // await pipeline(csvStringify.pipe(outputFileStream));
-    // Use pipeline instead of manual piping
-    // try {
-    //     await pipeline(reader, csvStringify, outputFileStream);
-    // } catch (err) {
-    //     console.log('Error in pipeline', err);
-    // }
+	await waitForWrite();
 
-	// const waitForCsvRead = () =>
-	// 	new Promise((resolve, reject) => {
-	// 		reader
-	// 			.on('end', () => {
-    //                 console.log('waitForCsvRead resolve');
-    //                 csvStringify.end();
-	// 				resolve({});
-	// 			})
-	// 			.on('error', (err) => {
-    //                 console.log('waitForCsvRead error', err);
-	// 				reject();
-	// 			});
-	// 	});
-
-	// await waitForCsvRead();
-    
-
-	// // Pipe the CSV formatted output to the file
-	// const waitForCsvWrite = () =>
-	// 	new Promise((resolve, reject) => {
-	// 		csvStringify
-	// 			.pipe(outputFileStream)
-	// 			.on('finish', () => {
-    //                 console.log('waitForCsvWrite resolve');
-	// 				resolve({});
-	// 			})
-	// 			.on('error', (err) => {
-    //                 console.log('waitForCsvWrite error', err);
-	// 				reject();
-	// 			});
-	// 	});
-
-	// await waitForCsvWrite();
-
+	console.log('Creating gz file from csv');
 	await gzipCsv(outputFileName, outputGzipFileName);
+	console.log('Completed creating gz file from csv');
+
+	console.log('Uploading gz file to S3');
+	await s3.send(
+		new PutObjectCommand({
+			Bucket: OUTPUT_BUCKET,
+			Key: outputFileName,
+			Body: fs.createReadStream(outputGzipFileName),
+			ContentType: 'application/gzip',
+		})
+	);
+	console.log('Completed uploading gz file to S3');
 
 	// Write and upload new shared mobile numbers
 	const mobileNumbersFileName = `mobile-numbers-${new Date().getFullYear()}-Q${Math.ceil((new Date().getMonth() + 1) / 3)}.csv.gz`;
 	const mobileNumbersStream = fs.createWriteStream(mobileNumbersFileName);
 	const gzipMobileStream = zlib.createGzip();
 
-	await pipeline(Readable.from(Array.from(newSharePids).map((pid) => `${pid},${allMobileNumbers.get(pid)}\n`)), gzipMobileStream, mobileNumbersStream);
+	await pipeline(
+		Readable.from(['pid,phone_1_dd\n', ...Array.from(newSharePids).map((pid) => `${pid},${allMobileNumbers.get(pid)}\n`)]),
+		gzipMobileStream,
+		mobileNumbersStream
+	);
 
 	await s3.send(
 		new PutObjectCommand({
@@ -233,8 +242,6 @@ async function processData(maxRecords?: number) {
 	);
 }
 
-processData(20000).catch(err => {
-    console.error(err);
+processData(100).catch((err) => {
+	console.error(err);
 });
-//
-// gzipCsv('dataset-2025-Q2.csv', 'dataset-2025-Q2.csv.gz');
