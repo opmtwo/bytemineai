429f74bc8c701728c8d42d8060675520b9728986
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-04-18 20:30:24 +0530

    added sample data

---
+++ data/es-request-input.tsx
@@ -0,0 +1,120 @@
+export const sampleRequest = {
+    "keywords": {
+        "include": [],
+        "exclude": []
+    },
+    "name_first": {
+        "include": [],
+        "exclude": []
+    },
+    "name_last": {
+        "include": [],
+        "exclude": []
+    },
+    "contactTitles": {
+        "include": [],
+        "exclude": []
+    },
+    "contactEducation": {
+        "include": [],
+        "exclude": []
+    },
+    "contactSkills": {
+        "include": [],
+        "exclude": []
+    },
+    "contactInterests": {
+        "include": [],
+        "exclude": []
+    },
+    "contactLevels": {
+        "include": [],
+        "exclude": []
+    },
+    "contactJobFunctions": {
+        "include": [],
+        "exclude": []
+    },
+    "hqLocations": {
+        "include": [],
+        "exclude": []
+    },
+    "cities": {
+        "include": [],
+        "exclude": []
+    },
+    "states": {
+        "include": [],
+        "exclude": []
+    },
+    "companyIndustries": {
+        "include": [],
+        "exclude": []
+    },
+    "companySizeIds": {
+        "include": [],
+        "exclude": []
+    },
+    "companyRevenueIds": {
+        "include": [],
+        "exclude": []
+    },
+    "companySicCodes": {
+        "include": [],
+        "exclude": []
+    },
+    "companyNaicsCodes": {
+        "include": [],
+        "exclude": []
+    },
+    "companyNames": {
+        "include": [],
+        "exclude": []
+    },
+    "uRLs": {
+        "include": [],
+        "exclude": []
+    },
+    "hasChangedJobRecently": false,
+    "urlsToSuppress": {
+        "include": [],
+        "exclude": []
+    },
+    "linkedinUrls": {
+        "include": [],
+        "exclude": []
+    },
+    "titlesToSuppress": {
+        "include": [],
+        "exclude": []
+    },
+    "emailsToSuppress": {
+        "include": [],
+        "exclude": []
+    },
+    "emailAddresses": {
+        "include": [],
+        "exclude": []
+    },
+    "requiredEmail": false,
+    "hasValidateEmails": false,
+    "hasPrimaryContactsOnly": false,
+    "personalEmailOnly": false,
+    "directDialOnly": false,
+    "hasPhone": false,
+    "hasWorkEmail": false,
+    "hasPersonalEmail": false,
+    "md5HashedEmailAddresses": {
+        "include": [],
+        "exclude": []
+    },
+    "facebookUrls": {
+        "include": [],
+        "exclude": []
+    },
+    "mobilePhones": {
+        "include": [],
+        "exclude": []
+    },
+    "audience": false
+}
