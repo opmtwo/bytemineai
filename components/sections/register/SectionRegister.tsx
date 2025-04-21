8662f3d46155b3e4ad41b9db942a8c7138bcff59
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-04-21 12:08:15 +0530

    backup and update SectionRegister

--- components/sections/register/SectionRegister.tsx
+++ components/sections/register/SectionRegister.tsx
@@ -1,31 +1,39 @@
-import { useState } from 'react';
+import { SignInOutput } from 'aws-amplify/auth';
 import { AnimatePresence } from 'framer-motion';
-import RegisterCodeForm from './RegisterCodeForm';
+import { useState } from 'react';
+import { UrlObject } from 'url';
+
+import RegisterCodeFormNew from './RegisterCodeFormNew';
 import RegisterMainForm from './RegisterMainForm';
 
 const SectionRegister = () => {
-
 	const [email, setEmail] = useState('');
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
-	const [activeForm, setActiveForm] = useState('main-form');
+	const [activeForm, setActiveForm] = useState<'main' | 'code'>('main');
 
 	const onMainFormSubmit = (email: string, username: string, password: string) => {
 		setEmail(email);
 		setUsername(username);
 		setPassword(password);
-		//setActiveForm('code-form');
+		setActiveForm('code');
+	};
 
-
+	const onCodeFormSubmit = async (user: SignInOutput, urlObject?: UrlObject) => {
+		// onSignIn(user);
+		window.location.href = '/';
 	};
 
 	const onCodeFormCancel = () => {
-		setActiveForm('main-form');
+		setActiveForm('main');
 	};
 
 	return (
 		<AnimatePresence>
-			{activeForm === 'main-form' && <RegisterMainForm onSubmit={onMainFormSubmit} />}
+			{activeForm === 'main' && <RegisterMainForm onSubmit={onMainFormSubmit} />}
+			{activeForm === 'code' && (
+				<RegisterCodeFormNew email={email} username={username} password={password} onSubmit={onCodeFormSubmit} onCancel={onCodeFormCancel} />
+			)}
 		</AnimatePresence>
 	);
 };
