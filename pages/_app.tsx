ab00daa6a181b70077d63c4e9af784d3a61c0954
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-04-21 11:42:34 +0530

    removed SettingsProvider, Script from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,18 @@
 import '../styles/index.scss';
 
-// import Amplify from "@aws-amplify/core";
 import { Amplify } from 'aws-amplify';
 import { fetchAuthSession } from 'aws-amplify/auth';
 import { LayoutGroup } from 'framer-motion';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import Script from 'next/script';
 import { ToastContainer } from 'react-toastify';
 
 import EnrichmentsInProgress from '../components/enrich/EnrichmentsInProgress';
 import Favicon from '../components/Favicon';
 import SettingsLoader from '../components/SettingsLoader';
 import AuthDataProvider from '../providers/auth-data-provider';
-import SettingsProvider from '../providers/settings-provider';
 import StripeProvider from '../providers/stripe-provider';
 import amplifyConfig from '../src/aws-exports';
-
-// Amplify.configure(amplifyConfig);
 
 Amplify.configure(amplifyConfig, {
 	API: {
@@ -45,27 +40,16 @@ const NewApp = ({ Component, pageProps }: AppProps) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-			        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-			        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-			        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-			        })(window,document,'script','dataLayer','GTM-MKDRX86');
-			      `}
-      </Script>
       <ToastContainer />
       <AuthDataProvider>
-        <SettingsProvider>
-          <StripeProvider>
-            <SettingsLoader />
-            <Favicon />
-            <LayoutGroup>
-              <Component {...pageProps} />
-              <EnrichmentsInProgress />
-            </LayoutGroup>
-          </StripeProvider>
-        </SettingsProvider>
+        <StripeProvider>
+          <SettingsLoader />
+          <Favicon />
+          <LayoutGroup>
+            <Component {...pageProps} />
+            <EnrichmentsInProgress />
+          </LayoutGroup>
+        </StripeProvider>
       </AuthDataProvider>
     </>
   );
