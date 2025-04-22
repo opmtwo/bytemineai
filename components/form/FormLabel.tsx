af18518ca55071e14b6967c85b53846a20cde4e7
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-04-22 10:22:05 +0530

    added required prop support to FormLabel

--- components/form/FormLabel.tsx
+++ components/form/FormLabel.tsx
@@ -1,14 +1,27 @@
 import classNames from 'classnames';
 
-const FormLabel = ({ label, labelFor, error, iconLabel }: { label?: string; labelFor?: string; error?: Error; iconLabel?: any }) => {
+const FormLabel = ({
+	label,
+	labelFor,
+	error,
+	iconLabel,
+	required,
+}: {
+	label?: string;
+	labelFor?: string;
+	error?: Error;
+	iconLabel?: any;
+	required?: boolean;
+}) => {
 	if (!label) {
 		return null;
 	}
-	const cssClassNames = classNames('label has-text-weight-semibold', { 'has-text-danger': error });
+	const cssClassNames = classNames('label has-text-weight-medium', { 'has-text-danger': error });
 	return (
 		<div>
-			<label className={cssClassNames} htmlFor={labelFor} style={{ fontSize: '0.88rem' }}>
+			<label className={cssClassNames} htmlFor={labelFor}>
 				{label}
+				{required ? <span className="has-text-info">*</span> : null}
 				<span className=" ml-2">{iconLabel}</span>
 			</label>
 		</div>
