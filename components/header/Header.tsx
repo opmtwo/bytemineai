04007cf518db3b1785e0ecc5df3edc62e43f7e7f
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-04-23 17:21:08 +0530

    updated Header ui

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,24 +5,37 @@ import HeaderMenu from './HeaderMenu';
 import HeaderMenuToggle from './HeaderMenuToggle';
 import HeaderQuickMenu from './HeaderQuickMenu';
 import HeaderLogo from './NavbarLogo';
+import { useAuthContext } from '../../providers/auth-data-provider';
 
 const Header = ({ onUpgrade }: { onUpgrade: () => void }) => {
 	const [isMenuActive, setIsMenuActive] = useState(false);
+
+	const { self, refresh } = useAuthContext();
+
+	const avatarUrl = self?.avatarS3Url ?? '/avatar.png';
 
 	const onMenuToggle = () => {
 		setIsMenuActive(!isMenuActive);
 	};
 
 	return (
-		<nav className="navbar has-shadow is-fixed-top is-spaced is-flex is-align-items-center" role="navigation" aria-label="main navigation">
-			<div className="navbar-brand is-flex is-align-items-center">
-				<HeaderLogo />
+		<nav className="navbar is-fixed-top is-flex is-align-items-center has-border" role="navigation" aria-label="main navigation">
+			<div className="navbar-start">
+				<div className="navbar-brand is-flex is-align-items-center is-justify-content-center">
+					<HeaderLogo />
+				</div>
 			</div>
-			<HeaderMenuToggle onToggle={onMenuToggle} />
+			{/* <HeaderMenuToggle onToggle={onMenuToggle} />
 			<div className={classNames('navbar-menu', { 'is-active': isMenuActive })}>
 				<HeaderMenu />
 			</div>
-			<HeaderQuickMenu />
+			<HeaderQuickMenu /> */}
+			<div className="navbar-end">
+				{/* <div className="is-clipped image is-48x48 has-radius is-rounded">
+					<img src={avatarUrl} alt="" />
+				</div> */}
+				<HeaderQuickMenu />
+			</div>
 		</nav>
 	);
 };
