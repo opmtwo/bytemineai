f27113bd12376ded888d038651b08a43e2f32a54
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-04-23 17:20:54 +0530

    updated HeaderQuickMenu ui

--- components/header/HeaderQuickMenu.tsx
+++ components/header/HeaderQuickMenu.tsx
@@ -1,19 +1,25 @@
 import { useRouter } from 'next/router';
 
 import { useAuthContext } from '../../providers/auth-data-provider';
-import { useSettingsContext } from '../../providers/settings-provider';
 import Anchor from '../Anchor';
 import FormButton from '../form/FormButton';
 import IconAvatar from '../icons/IconAvatar';
 
 const HeaderQuickMenu = () => {
 	const { user, isAdmin, isRoot, attributes } = useAuthContext();
-	const { canUpgrade } = useSettingsContext();
+	// const { canUpgrade } = useSettingsContext();
 	const router = useRouter();
 
+	const { self, refresh } = useAuthContext();
+
+	const avatarUrl = self?.avatarS3Url ?? '/avatar.png';
+
+	const canUpgrade = false;
+
 	const onUpgrade = async () => {
-		await router.push('/account-settings/subscription-billing/plan');
+		await router.push('/settings/subscription');
 	};
+
 	return (
 		<div className="navbar-item navbar-quickmenu">
 			{canUpgrade && (
@@ -22,33 +28,39 @@ const HeaderQuickMenu = () => {
 				</FormButton>
 			)}
 			<div className="navbar-item has-dropdown is-hoverable">
-				<FormButton icon={<IconAvatar />} size="is-medium" variant={['is-icon', 'is-rounded', 'is-outlined']} className="ml-5" color="is-primary" />
+				{/* <FormButton icon={<IconAvatar />} size="is-medium" variant={['is-icon', 'is-rounded', 'is-outlined']} className="ml-5" color="is-primary" /> */}
+				<div className="is-clipped image is-48x48 has-radius is-rounded">
+					<img src={avatarUrl} alt="" style={{ maxHeight: 'none' }} />
+				</div>
+
 				<div className="navbar-dropdown is-right">
 					<div className="navbar-dropdown-card">
-						<div className="navbar-item pb-0" style={{ height: 'auto' }}>
-							<strong>
-								<small>
-									{attributes?.given_name} {attributes?.family_name}
+						<div className="navbar-item mb-3" style={{ height: 'auto', lineHeight: 1 }}>
+							<strong className="has-text-grey">
+								<small className="has-text-dark">
+									{self?.name || `${self?.givenName || ''} ${self?.familyName || ''}`}
+									<br />
+									<small className="has-text-grey">{self?.email}</small>
 								</small>
 							</strong>
 						</div>
 
 						{isAdmin || isRoot ? (
-							<Anchor href="/account-settings" className="navbar-item">
+							<Anchor href="/settings/account" className="navbar-item py-3">
 								Account Settings
 							</Anchor>
 						) : null}
 
 						{isRoot ? (
-							<Anchor href="/admin-panel" className="navbar-item">
+							<Anchor href="/admin-panel" className="navbar-item py-3">
 								Admin Panel
 							</Anchor>
 						) : null}
 						{/*
-						<Anchor href="/my-contacts" className="navbar-item">
+						<Anchor href="/my-contacts" className="navbar-item py-3">
 							All Contacts
 						</Anchor>*/}
-						<Anchor href="/logout" className="navbar-item has-text-weight-normal">
+						<Anchor href="/logout" className="navbar-item py-3 has-text-weight-normal">
 							Log out
 						</Anchor>
 					</div>
