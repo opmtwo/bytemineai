4cb6c91c9026755f978789aee258c415b6f3c1d6
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-04-24 20:57:35 +0530

    updated amplify backend config

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -28,6 +28,13 @@ export type AmplifyDependentResourcesAttributes = {
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
       "Region": "string"
+    },
+    "bytemineaiEnrichLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   },
   "storage": {
