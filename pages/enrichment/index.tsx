2e4f4d857217f00ed8d3595af097cf1c34a70758
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-05-02 14:05:02 +0530

    added enrichment page

---
+++ pages/enrichment/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+import PageLayout from '../../components/layouts/PageLayout';
+import SectionEnrichments from '../../components/sections/enrichment/SectionEnrichments';
+import UserGuard from '../../guards/UserGuard';
+import CrudProvider from '../../providers/crud-provider';
+import awsmobile from '../../src/aws-exports';
+
+const EnrichmentsIndex = () => {
+	return (
+		<>
+			<Head>
+				<title>Bulk Enrichment</title>
+				<meta name="description" content="" />
+			</Head>
+			<PageLayout>
+				<CrudProvider baseApi={awsmobile.aws_cloud_logic_custom[0].name} baseUrl="api/v1/enrichments" searchKeys={['name', 'slug']} baseSortMap={[]}>
+					<SectionEnrichments />
+				</CrudProvider>
+			</PageLayout>
+		</>
+	);
+};
+
+export default EnrichmentsIndex;
