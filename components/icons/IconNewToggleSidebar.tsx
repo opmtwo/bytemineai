533ae65a78943f281ce5a9263c1c39996cd725fe
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-05-02 13:59:58 +0530

    fixed icons

--- components/icons/IconNewToggleSidebar.tsx
+++ components/icons/IconNewToggleSidebar.tsx
@@ -2,7 +2,7 @@ import { SVGProps } from 'react';
 
 const IconNewToggleSidebar = ({ stroke = '#475467', ...props }: SVGProps<SVGSVGElement>) => (
 	<svg viewBox="0 0 21 21" {...props}>
-		<g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(1 2)">
+		<g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" transform="translate(1 2)">
 			<path strokeWidth={2} d="m.5.5v16.021" />
 			<path strokeWidth={2} d="m18.5.5v16.021" />
 			<path strokeWidth={2} d="m12.507 12.515 4-4-4-4.015" />
