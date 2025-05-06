7caf1e1b4e02090b7f30b6f0e4bf0160fa5f8b63
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-05-06 13:54:06 +0530

    backup and updated home

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+import PageLayout from '../components/layouts/PageLayout';
+import SectionProspects from '../components/sections/prospects/SectionProspects';
+import UserGuard from '../guards/UserGuard';
+
+const Home = () => {
+	return (
+		<UserGuard>
+			<Head>
+				<title>Dashboard</title>
+				<meta name="description" content="" />
+			</Head>
+			<PageLayout>
+				<SectionProspects />
+			</PageLayout>
+		</UserGuard>
+	);
+};
+
+export default Home;
