be1880e02eb361a820be6af61d47fba3c452a33a
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-05-10 13:06:55 +0530

    added comments to employee sizes api

--- bytemine/bytemine-employee-sizes.tsx
+++ bytemine/bytemine-employee-sizes.tsx
@@ -2,10 +2,10 @@ export enum BytemineEmployeeSizes {
 	'1 to 19' = '1 to 19',
 	'20 to 99' = '20 to 99',
 	'100 to 499' = '100 to 499',
-	'500 to 999' = '500 to 999',
+	'500 to 999' = '500 to 999', // missing?
 	'1000 to 4999' = '1000 to 4999',
 	'5000+' = '5000+',
-	'10000+' = '10000+',
+	'10000+' = '10000+', // missing?
 }
 
 export const BytemineEmployeeSizeOptions = Object.entries(BytemineEmployeeSizes)
