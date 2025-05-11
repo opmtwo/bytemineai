8028173373008721d8a9bc55c2c02c4102b43eb0
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-05-12 02:26:37 +0530

    Updated ContactEmailStatus as per new ui

--- components/ContactEmailStatus.tsx
+++ components/ContactEmailStatus.tsx
@@ -1,17 +1,44 @@
-import { useEffect, useState } from 'react';
+import { HTMLAttributes, ReactNode, useEffect, useState } from 'react';
 
-import { IBytemineContact } from '../types';
-import { getEmailValidityStatus } from '../utils/contact-utilsx';
+import { atDataResponseCodes } from '../consts';
+import classNames from 'classnames';
 
-const ContactEmailStatus = ({ contact }: { contact: IBytemineContact }) => {
-	const [status, setStatus] = useState('');
+interface ContactEmailStatusProps extends HTMLAttributes<HTMLSpanElement> {
+	statusCode: string;
+}
+
+const ContactEmailStatus = ({ statusCode, className = 'ml-2' }: ContactEmailStatusProps) => {
+	const [status, setStatus] = useState<ReactNode>();
 
 	useEffect(() => {
-		const newStatus = getEmailValidityStatus(contact);
+		const newStatus = getEmailValidityStatus(statusCode);
 		setStatus(newStatus);
-	}, [contact]);
+	}, [statusCode]);
 
-	return <span>{status}</span>;
+	const getCodeInfo = (status: string) => {
+		if (['valid'].includes(status)) {
+			return { bg: '#edfcf2', fg: '#099250', value: 'Valid' };
+		}
+		if (['unknown', 'unverifiable'].includes(status)) {
+			return { bg: '#fff4e2', fg: '#db9729', value: 'Accept-All' };
+		}
+		return { bg: '#fff0f1', fg: '#ed1a15', value: 'Invalid' };
+	};
+
+	const getEmailValidityStatus = (code: string) => {
+		if (!code) {
+			return null;
+		}
+		const status = (atDataResponseCodes as any)[code]?.status;
+		const { bg, fg, value } = getCodeInfo(status);
+		return (
+			<span className={classNames('has-radius px-3 py-1', className)} style={{ backgroundColor: bg, color: fg }}>
+				{value}
+			</span>
+		);
+	};
+
+	return status;
 };
 
 export default ContactEmailStatus;
