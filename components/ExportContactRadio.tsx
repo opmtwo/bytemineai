b1ed0b25a0b449c231ac26242ebaf9a289b2479f
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-05-11 12:32:21 +0530

    updated ExportContactRadio ui

--- components/ExportContactRadio.tsx
+++ components/ExportContactRadio.tsx
@@ -21,17 +21,18 @@ const ExportContactRadio = ({
 		onSelect(e.target.value);
 	};
 
-	const cssClasses = 'button is-block is-relative is-inverted is-ui-button is-size-6';
+	const cssClasses = 'is-block is-relative is-inverted is-ui-button is-size-6 has-radius has-text-centered';
 
 	const style: CSSProperties = {
 		height: 'initial',
-		padding: '3rem 0.5rem 1rem',
+		padding: '1.25rem 0.5rem 2rem',
 		whiteSpace: 'initial',
-		border: '1px solid'
+		border: '1px solid',
+		borderColor: value === checkedValue ? '#84caff' : 'currentcolor',
 	};
 
 	return (
-		<label htmlFor={id} className={classNames(cssClasses, value === checkedValue ? 'is-primary has-text-primary' : 'has-text-grey')} style={style}>
+		<label htmlFor={id} className={classNames(cssClasses, value === checkedValue ? 'is-primary has-text-info' : 'has-text-grey')} style={style}>
 			<input id={id} type="radio" name={name} className="is-hidden" value={value} onChange={handleSelect} />
 			{children}
 		</label>
