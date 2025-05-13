74670cab0f1833a2fef717276869e6fdb30aa532
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2025-05-13 18:10:13 +0530

    added live api data in TrialNotice

--- components/TrialNotice.tsx
+++ components/TrialNotice.tsx
@@ -5,14 +5,14 @@ import { useAuthContext } from '../providers/auth-data-provider';
 import { AccountType } from '../types';
 import CardAnimatePresence from './cards/CardAnimatePresence';
 import FormButton from './form/FormButton';
-import IconClock from './icons/IconClock';
 import FormButtonNew from './form/FormButtonNew';
+import IconClock from './icons/IconClock';
 
 const TrialNotice = ({ onCustomize }: { onCustomize: () => void }) => {
 	const [message, setMessage] = useState<string>();
 	const [creditsLeft, setCreditsLeft] = useState(0);
 
-	const { sub, team, isRoot } = useAuthContext();
+	const { team, isTrial, subscription } = useAuthContext();
 
 	useEffect(() => {
 		if (!team?.id) {
@@ -20,42 +20,45 @@ const TrialNotice = ({ onCustomize }: { onCustomize: () => void }) => {
 		}
 		(async () => {
 			try {
-				const now = moment();				
+				const now = moment();
 				const then = moment.unix(parseInt(team?.createdAt || '') / 1000).add(1, 'days');
 				setCreditsLeft(team?.currentCredits || 0);
-				if (now > then || sub?.currentCredits === 0) {
-                    setMessage('Upgrade now and save 15%, use code NYMBLR15 at checkout.');
-				} else {
-					const diffDates=then.diff(now)
-					const timeData=moment.duration(diffDates);
-		            const days=timeData.days();
-					const hours=timeData.hours();
-					const minutes=timeData.minutes();
-				
-					let message="Upgrade within ";
 
-					if(days > 0){
-						message=`${message} ${days} days `;
-					}
-					if(hours > 0){
-						message=`${message} ${hours} hours `;
-					}
-					if(minutes > 0){
-						message=`${message} ${minutes} minutes `;
-					}
-                    message=`${message} and save 20%, use code NYMBLR20 at checkout.`
-    				setMessage(message);
+				if (now > then && (subscription?.currentCredits || 50) <= 0) {
+					setMessage('Upgrade now and save 15%, use code NYMBLR15 at checkout.');
+					return;
 				}
+
+				const diffDates = then.diff(now);
+				const timeData = moment.duration(diffDates);
+				const days = timeData.days();
+				const hours = timeData.hours();
+				const minutes = timeData.minutes();
+
+				let message = 'Upgrade within ';
+
+				if (days > 0) {
+					message = `${message} ${days} days `;
+				}
+
+				if (hours > 0) {
+					message = `${message} ${hours} hours `;
+				}
+
+				if (minutes > 0) {
+					message = `${message} ${minutes} minutes `;
+				}
+
+				message = `${message} and save 20%, use code NYMBLR20 at checkout.`;
+				setMessage(message);
 			} catch (err) {
 				console.log('Error fetching usage details', err);
 			}
 		})();
 	}, [team?.id]);
-    
-	const isActive = !isRoot && sub?.subscriptionStatus !== AccountType.Trial && message !== undefined;
 
 	return (
-		<CardAnimatePresence isActive={isActive}>
+		<CardAnimatePresence isActive={subscription?.id !== undefined && isTrial ? true : false}>
 			<div
 				className="box p-5 is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-between has-border-primary"
 				style={{ borderWidth: 1, borderStyle: 'solid', marginBottom: 20 }}
